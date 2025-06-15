e00be056d47058596ef80bf565ab92d596a71685
RamonGB17 <145658490+RamonGB17@users.noreply.github.com>
2025-06-15 17:58:54 +0200

    Delete src directory

--- src/main/java/org/example/triviadexbackend/repository/TriviaOptionRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.triviadexbackend.repository;
-
-import org.example.triviadexbackend.entity.TriviaOption;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TriviaOptionRepository extends JpaRepository<TriviaOption, Long> {
-}
